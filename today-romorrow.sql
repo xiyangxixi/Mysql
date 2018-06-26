96116ce62951d0aa964f4a30dbd7ea7902fd6616
xiyangxixi <34201743+xiyangxixi@users.noreply.github.com>
2018-06-26 15:55:59 +0800

    以当前日期创建明天日期为表名的表

---
+++ today-romorrow.sql
@@ -0,0 +1,24 @@
+# 使用今天的日期创建明天的表 如今天是2018-06-26，则表名为2018-06-27
+
+BEGIN
+	 declare the_date date default CURDATE();
+		declare cd date;
+		declare ld date;
+		select curdate() into cd;
+		select last_day(curdate()) into ld;
+		#set the_date="2018-06-30";
+	if the_date=cd and the_date!=ld THEN
+    #select the_date;
+		SET @sqlstr = CONCAT('create table TABLE_',DATE_FORMAT(CURDATE(),'%Y%m%d'),' (id int)');
+ 
+    PREPARE stmt1 FROM @sqlstr ;
+ 
+    EXECUTE stmt1 ;
+	ELSE
+		SET @sqlstr = CONCAT('create table TABLE_',DATE_FORMAT(date_add(curdate() - day(curdate()) + 1, interval 1 month),'%Y%m%d'),' (id int)');
+ 
+    PREPARE stmt1 FROM @sqlstr ;
+ 
+    EXECUTE stmt1 ; 
+	end if;
+END
